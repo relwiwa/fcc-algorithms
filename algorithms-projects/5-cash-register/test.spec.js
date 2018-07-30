7231c0897b1648c7d77849f61cc246221254fba2
Walter <relw1wa-github@yahoo.de>
2018-07-30 09:25:38 +0100

    Cash Register: Setup test and algorithm file

---
+++ algorithms-projects/5-cash-register/test.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+const algorithm = require('./algorithm');
+
+describe('"Cash Register" algorithm', () => {
+  describe('concerning its input and output', () => {  });
+});
